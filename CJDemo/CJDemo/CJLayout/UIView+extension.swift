130a1ef4e8ed942a9887e9442f42bbed9375d7ca
caoguangming <caoguangmingcn@gmail.com>
2019-04-16 11:15:35 +0800

    1
    完善布局

--- CJDemo/CJDemo/CJLayout/UIView+extension.swift
+++ CJDemo/CJDemo/CJLayout/UIView+extension.swift
@@ -17,7 +17,6 @@ extension UIView {
         }
     }
     
-    
     var cj_safe_topAnchor: NSLayoutYAxisAnchor {
         if #available(iOS 11.0, *) {
             return self.safeAreaLayoutGuide.topAnchor
@@ -42,65 +41,62 @@ extension UIView {
         }
     }
     
-    
-    
-    
-//    var cj_safe_leadingAnchor: NSLayoutXAxisAnchor {
-//        if #available(iOS 11.0, *) {
-//            return self.safeAreaLayoutGuide.leadingAnchor
-//        } else {
-//            return self.leadingAnchor
-//        }
-//    }
-//
-//    var cj_safe_trailingAnchor: NSLayoutXAxisAnchor {
-//        if #available(iOS 11.0, *) {
-//            return self.safeAreaLayoutGuide.trailingAnchor
-//        } else {
-//            return self.trailingAnchor
-//        }
-//    }
-//
-//    var cj_safe_leftAnchor: NSLayoutXAxisAnchor {
-//        if #available(iOS 11.0, *) {
-//            return self.safeAreaLayoutGuide.leftAnchor
-//        } else {
-//            return self.leftAnchor
-//        }
-//    }
-//
-//    var cj_safe_rightAnchor: NSLayoutXAxisAnchor {
-//        if #available(iOS 11.0, *) {
-//            return self.safeAreaLayoutGuide.rightAnchor
-//        } else {
-//            return self.rightAnchor
-//        }
-//    }
-//
-//
-//
-//    var cj_safe_widthAnchor: NSLayoutDimension {
-//        if #available(iOS 11.0, *) {
-//            return self.safeAreaLayoutGuide.widthAnchor
-//        } else {
-//            return self.widthAnchor
-//        }
-//    }
-//
-//    var cj_safe_centerXAnchor: NSLayoutXAxisAnchor {
-//        if #available(iOS 11.0, *) {
-//            return self.safeAreaLayoutGuide.centerXAnchor
-//        } else {
-//            return self.centerXAnchor
-//        }
-//    }
-//
-//    var cj_safe_centerYAnchor: NSLayoutYAxisAnchor {
-//        if #available(iOS 11.0, *) {
-//            return self.safeAreaLayoutGuide.centerYAnchor
-//        } else {
-//            return self.centerYAnchor
-//        }
-//    }
+    var cj_safe_leadingAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.leadingAnchor
+        } else {
+            return self.layoutMarginsGuide.leadingAnchor
+        }
+    }
+
+    var cj_safe_trailingAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.trailingAnchor
+        } else {
+            return self.layoutMarginsGuide.trailingAnchor
+        }
+    }
+
+    var cj_safe_leftAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.leftAnchor
+        } else {
+            return self.layoutMarginsGuide.leftAnchor
+        }
+    }
+
+    var cj_safe_rightAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.rightAnchor
+        } else {
+            return self.layoutMarginsGuide.rightAnchor
+        }
+    }
+
+
+
+    var cj_safe_widthAnchor: NSLayoutDimension {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.widthAnchor
+        } else {
+            return self.layoutMarginsGuide.widthAnchor
+        }
+    }
+
+    var cj_safe_centerXAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.centerXAnchor
+        } else {
+            return self.centerXAnchor
+        }
+    }
+
+    var cj_safe_centerYAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return self.safeAreaLayoutGuide.centerYAnchor
+        } else {
+            return self.centerYAnchor
+        }
+    }
     
 }
